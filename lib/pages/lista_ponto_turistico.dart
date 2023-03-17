2dd63ea723e0719ff9c3f10b3ef5a87cd9cd58c0
FelipeZawadski <62263292+FelipeZawadski@users.noreply.github.com>
2023-03-17 20:06:15 -0300

    Primeiro Commit

---
+++ lib/pages/lista_ponto_turistico.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
+class ListaPontoTuristicoPage extends StatefulWidget{
+
+  @override
+  _ListaPontoTuristicoPageState createState() =>_ListaPontoTuristicoPageState();
+}
+
+class _ListaPontoTuristicoPageState extends State<ListaPontoTuristicoPage>{
+
+}
