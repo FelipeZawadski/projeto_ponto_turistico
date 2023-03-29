1c67d5f898034b4f0b5eca32a4d69f48b45508f0
FelipeZawadski <62263292+FelipeZawadski@users.noreply.github.com>
2023-03-29 15:27:57 -0300

    Segundo Commit

---
+++ lib/model/ponto_turistico.dart
@@ -0,0 +1,43 @@
+
+import 'package:intl/intl.dart';
+
+class PontoTuristico{
+
+  static const CAMPO_ID = 'id';
+  static const CAMPO_NOME = 'nome';
+  static const CAMPO_DESCRICAO = 'descricao';
+  static const CAMPO_DIFERENCIAIS = 'diferenciais';
+  static const DATA = 'data';
+  static const NOME_TABLE = 'pontosturisticos';
+
+  int id;
+  String nome;
+  String descricao;
+  String diferenciais;
+  DateTime? data;
+
+  PontoTuristico({required this.id, required this.nome ,required this.descricao, required this.diferenciais, required this.data});
+
+  String get dataFormatada{
+    if(data == null){
+      return "";
+    }
+    return DateFormat('dd/MM/yyyy').format(data!);
+  }
+
+  Map<String, dynamic> toMap() => <String, dynamic>{
+    CAMPO_ID: id,
+    CAMPO_NOME: nome,
+    CAMPO_DESCRICAO: descricao,
+    CAMPO_DIFERENCIAIS: diferenciais,
+    DATA: data == null ? null : DateFormat("dd/MM/yyyy").format(data!),
+  };
+
+  factory PontoTuristico.fromMap(Map<String, dynamic>  map) => PontoTuristico(
+      id: map[CAMPO_ID] is int ? map[CAMPO_ID] : null,
+      nome: map[CAMPO_NOME] is String ? map[CAMPO_NOME] : '',
+      descricao: map[CAMPO_DESCRICAO] is String ? map[CAMPO_DESCRICAO] : '',
+      diferenciais: map[CAMPO_DIFERENCIAIS] is String ? map[CAMPO_DIFERENCIAIS] : '',
+      data: map[DATA] == null ? null : DateFormat("dd/MM/yyyy").parse(map[DATA])
+  );
+}
--- lib/pages/lista_ponto_turistico.dart
+++ lib/pages/lista_ponto_turistico.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import '../model/ponto_turistico.dart';
 
 
 class ListaPontoTuristicoPage extends StatefulWidget{
@@ -10,4 +11,97 @@ class ListaPontoTuristicoPage extends StatefulWidget{
 
 class _ListaPontoTuristicoPageState extends State<ListaPontoTuristicoPage>{
 
+  static const ACAO_EDITAR = 'editar';
+  static const ACAO_EXCLUIR = 'excluir';
+
+  final pontosTuristicos = <PontoTuristico>[
+    PontoTuristico(id: 1, nome: 'Igreja', descricao: 'Igreja Matriz', diferenciais: 'Centro', data: DateTime.now())
+  ];
+
+  int _ultimo = 0;
+
+  @override
+  Widget build(BuildContext context){
+      return Scaffold(
+        appBar: _criarAppBar(),
+        body: _criarBody(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: (){},//_abrirCadastro,
+          tooltip: 'Novo Ponto Turistico',
+          child: Icon(Icons.add),
+        ),
+      );
+  }
+
+  AppBar _criarAppBar(){
+    return AppBar(
+      title: Text('Pontos Turisticos'),
+      actions: [
+        IconButton(onPressed: () {}, icon: Icon(Icons.filter_list)), // _paginaFiltro
+      ],
+    );
+  }
+
+  Widget _criarBody() {
+    if (pontosTuristicos.isEmpty) {
+      return const Center(
+        child: Text('Nenhum ponto turistico foi cadastrado',
+          style: TextStyle(fontSize: 23, fontWeight: FontWeight.bold),
+        ),
+      );
+    } else {
+      return ListView.separated(
+          itemBuilder: (BuildContext context, int index) {
+            final pontoTuristico = pontosTuristicos[index];
+            return PopupMenuButton<String>(
+                child: ListTile(title: Text('${pontoTuristico.id} - ${pontoTuristico.nome}'),
+                subtitle: Text('${pontoTuristico.descricao} - ${pontoTuristico.diferenciais} - Data - ${pontoTuristico.data} -'),),
+                itemBuilder: (BuildContext context) => criarItensMenuPopup(),
+                onSelected: (String valorSelecionado){
+                  /*if(valorSelecionado == ACAO_EDITAR){
+                    _abrirForm(pontoAtual: pontoTuristico, index: index);
+                  } else{
+                    _excluir(index);
+                  }*/
+                },
+            );
+          },
+        separatorBuilder: (BuildContext context, int index) => Divider(),
+        itemCount: pontosTuristicos.length);
+    }
+  }
+
+  /*void _paginaFiltro(){
+    final navigator = Navigator.of(context);
+    navigator.pushNamed(FiltroPage.ROUTE_NAME).then((alterouValores) {
+      if(alterouValores == true){
+        //////
+      }
+    });
+  }*/
+
+  List<PopupMenuEntry<String>> criarItensMenuPopup(){
+    return [
+      PopupMenuItem<String>(
+          value: ACAO_EDITAR,
+          child: Row(
+              children: [
+                Icon (Icons.edit, color: Colors.black),
+                Padding(padding: EdgeInsets.only(left: 10),
+                    child: Text('Editar')),
+              ]
+          )
+      ),
+      PopupMenuItem<String>(
+          value: ACAO_EXCLUIR,
+          child: Row(
+              children: [
+                Icon (Icons.delete, color: Colors.red),
+                Padding(padding: EdgeInsets.only(left: 10),
+                    child: Text('Excluir')),
+              ]
+          )
+      )
+    ];
+  }
 }
