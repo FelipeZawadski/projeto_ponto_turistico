e297763148eed0b17c9a3eed1c0e68fad41c780a
FelipeZawadski <62263292+FelipeZawadski@users.noreply.github.com>
2023-04-14 17:19:23 -0300

    Criação do banco de dados

--- lib/pages/lista_ponto_turistico.dart
+++ lib/pages/lista_ponto_turistico.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:projeto_ponto_turistico/pages/visualizar_ponto_turistico_page.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import '../dao/ponto_turistico_dao.dart';
 import '../model/ponto_turistico.dart';
 import '../widget/conteudo_form_dialog.dart';
 import 'filtro_page.dart';
@@ -17,11 +20,20 @@ class _ListaPontoTuristicoPageState extends State<ListaPontoTuristicoPage>{
   static const ACAO_EXCLUIR = 'excluir';
   static const ACAO_VISUALIZAR = 'visualizar';
 
-  final pontosTuristicos = <PontoTuristico>[
+  /*final pontosTuristicos = <PontoTuristico>[
     PontoTuristico(id: 1, nome: 'Igreja', descricao: 'Igreja Matriz', diferenciais: 'Centro', data: DateTime.now())
-  ];
+  ];*/
 
-  int _ultimoId = 0;
+  final _pontoTuristico = <PontoTuristico>[];
+  final _dao = PontoTuristicoDao();
+  var _carregando = false;
+
+
+  @override
+  void initState(){
+    super.initState();
+    _atualizarLista();
+  }
 
   @override
   Widget build(BuildContext context){
@@ -45,36 +57,66 @@ class _ListaPontoTuristicoPageState extends State<ListaPontoTuristicoPage>{
     );
   }
 
-  Widget _criarBody() {
-    if (pontosTuristicos.isEmpty) {
+  Widget _criarBody(){
+    if(_carregando){
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Align(
+            alignment: AlignmentDirectional.center,
+            child: CircularProgressIndicator(),
+          ),
+          Align(
+            alignment: AlignmentDirectional.center,
+            child: Padding(
+              padding: EdgeInsets.only(top: 10),
+              child: Text('Carregando seus pontos turisticos',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Theme.of(context).primaryColor,
+                ),
+              ),
+            ),
+          )
+        ],
+      );
+    }
+    if(_pontoTuristico.isEmpty){
       return const Center(
-        child: Text('Nenhum ponto turistico foi cadastrado',
-          style: TextStyle(fontSize: 23, fontWeight: FontWeight.bold),
+        child: Text('Nenhum ponto turistico cadastrado',
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
         ),
       );
-    } else {
-      return ListView.separated(
-          itemBuilder: (BuildContext context, int index) {
-            final pontoTuristico = pontosTuristicos[index];
-            return PopupMenuButton<String>(
-                child: ListTile(title: Text('${pontoTuristico.id} - ${pontoTuristico.nome}'),
-                subtitle: Text('${pontoTuristico.descricao} - ${pontoTuristico.diferenciais} - Data - ${pontoTuristico.dataFormatada}'),),
-                itemBuilder: (BuildContext context) => criarItensMenuPopup(),
-                onSelected: (String valorSelecionado){
-                  if(valorSelecionado == ACAO_EDITAR){
-                    _abrirCadastro(pontoTuristicoAtual: pontoTuristico, index: index);
-                  } else if(valorSelecionado == ACAO_VISUALIZAR){
-                    _abrirVisualizacao(pontoTuristicoAtual: pontoTuristico, index: index);
-                  }
-                  else{
-                    _excluir(index);
-                  }
-                },
-            );
-          },
-        separatorBuilder: (BuildContext context, int index) => Divider(),
-        itemCount: pontosTuristicos.length);
     }
+    return ListView.separated(
+      itemBuilder: (BuildContext context, int index){
+        final pontoTuristico = _pontoTuristico[index];
+        return PopupMenuButton<String>(
+          child: ListTile(
+            title: Text('${pontoTuristico.id} - ${pontoTuristico.descricao}',
+              style: TextStyle(
+              ),
+            ),
+          ),
+          itemBuilder: (BuildContext context) => criarItensMenuPopup(),
+          onSelected: (String valorSelecionado){
+            if (valorSelecionado == ACAO_EDITAR){
+              _abrirCadastro(pontoTuristico: pontoTuristico);
+            }else if (valorSelecionado == ACAO_VISUALIZAR){
+              Navigator.of(context).push(MaterialPageRoute(
+                builder: (_) => VisualizarPontoTuristicoPage(pontoTuristico: pontoTuristico),
+              ));
+            }
+            else{
+              _excluir(pontoTuristico);
+            }
+          },
+        );
+      },
+      separatorBuilder: (BuildContext context, int index) => Divider(),
+      itemCount: _pontoTuristico.length,
+    );
   }
 
   void _paginaFiltro(){
@@ -121,7 +163,34 @@ class _ListaPontoTuristicoPageState extends State<ListaPontoTuristicoPage>{
     ];
   }
 
-  void _abrirCadastro({PontoTuristico? pontoTuristicoAtual, int? index}){
+  void _atualizarLista() async {
+      setState(() {
+        _carregando = true;
+      });
+
+      final prefs = await SharedPreferences.getInstance();
+      final campoOrdenacao = prefs.getString(FiltroPage.CAMPO_ORDENACAO) ?? PontoTuristico.CAMPO_ID;
+      final usarOrdemDecrescente = prefs.getBool(FiltroPage.CAMPO_ORDEM_DECRECENTE) == true;
+      final filtroNome = prefs.getString(FiltroPage.CAMPO_NOME) ?? '';
+
+      final pontoTuristico = await _dao.listar(
+        filtro: filtroNome,
+        campoOrdenacao: campoOrdenacao,
+        usarOrdemDecrescente: usarOrdemDecrescente,
+      );
+
+      setState(() {
+        _pontoTuristico.clear();
+        if (pontoTuristico.isNotEmpty) {
+          _pontoTuristico.addAll(pontoTuristico);
+        }
+      });
+      setState(() {
+        _carregando = false;
+      });
+  }
+
+  void _abrirCadastro({PontoTuristico? pontoTuristicoAtual}){
     final key = GlobalKey<ConteudoFormDialogState>();
     showDialog(
         context: context,
---
+++ lib/pages/visualizar_ponto_turistico_page.dart
@@ -0,0 +1,98 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:projeto_ponto_turistico/model/ponto_turistico.dart';
+
+class VisualizarPontoTuristicoPage extends StatefulWidget {
+  final PontoTuristico pontoTuristico;
+
+  const VisualizarPontoTuristicoPage({Key? key, required this.pontoTuristico}) : super(key: key);
+
+  @override
+  _VisualizarPontoTuristicoPageState createState() => _VisualizarPontoTuristicoPageState();
+}
+
+class _VisualizarPontoTuristicoPageState extends State<VisualizarPontoTuristicoPage> {
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Detalhes dos Pontos Turisticos'),
+      ) ,
+      body: _criaBody(),
+    );
+  }
+
+  Widget _criaBody(){
+    return Padding(
+      padding: EdgeInsets.all(10),
+      child: ListView(
+        children: [
+          Row(
+            children: [
+              Campo(descricao:'Código: '),
+              Valor(valor: '${widget.pontoTuristico.id}'),
+            ],
+          ),
+          Row(
+            children: [
+              Campo(descricao:'Nome: '),
+              Valor(valor: '${widget.pontoTuristico.nome}'),
+            ],
+          ),
+          Row(
+            children: [
+              Campo(descricao:'Descrição: '),
+              Valor(valor: '${widget.pontoTuristico.descricao}'),
+            ],
+          ),
+          Row(
+            children: [
+              Campo(descricao:'Diferenciais: '),
+              Valor(valor: '${widget.pontoTuristico.diferenciais}'),
+            ],
+          ),
+          Row(
+            children: [
+              Campo(descricao:'Prazo: '),
+              Valor(valor: '${widget.pontoTuristico.data}'),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Campo extends StatelessWidget{
+  final String descricao;
+
+  const Campo({Key? key,required this.descricao}) : super(key: key);
+
+  @override
+  Widget build (BuildContext context){
+    return Expanded(
+      flex: 1,
+      child: Text(descricao,
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+}
+
+class Valor extends StatelessWidget{
+  final String valor;
+
+  const Valor({Key? key,required this.valor}) : super(key: key);
+
+  @override
+  Widget build (BuildContext context){
+    return Expanded(
+      flex: 4,
+      child: Text(valor),
+    );
+  }
+}
